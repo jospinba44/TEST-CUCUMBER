a982dca2033445d8c735afea7aac4a037d476d87
jospinba44 <jospinba44@gmail.com>
2025-01-20 21:28:33 +0200

    Integration continue test in progress

--- utils/db.js
+++ utils/db.js
@@ -1,30 +1,33 @@
-const { createPool } = require('mysql2/promise')
-const dotenv = require('dotenv')
+const { createPool } = require("mysql2/promise");
+const dotenv = require("dotenv");
 
-dotenv.config()
-var globalPool = undefined
+dotenv.config();
+var globalPool = undefined;
 const connection = async () => {
-          try {
-                    if(globalPool) return globalPool
-                    globalPool = await createPool({
-                              host: process.env.BD_HOST,
-                              user: process.env.DB_USER,
-                              password: process.env.DB_PASSWORD,
-                              database: process.env.DB_NAME,
-                              port: process.env.DB_PORT ? process.env.DB_PORT :3306
-                    })
-                    return globalPool
-          } catch (error) {
-                    throw error
-          }
-}
+  try {
+    if (globalPool) return globalPool;
+    globalPool = await createPool({
+      host: process.env.BD_HOST || "localhost",
+      user: process.env.DB_USER || "user",
+      password: process.env.DB_PASSWORD || "password",
+      database: process.env.DB_NAME || "test_db",
+      port: process.env.DB_PORT ? process.env.DB_PORT : 3306,
+      waitForConnections: true,
+      connectionLimit: 10,
+      queueLimit: 0,
+    });
+    return globalPool;
+  } catch (error) {
+    throw error;
+  }
+};
 
 const query = async (query, values) => {
-          const pool = await connection()
-          return ( await pool.query(query, values))[0]
-}
+  const pool = await connection();
+  return (await pool.query(query, values))[0];
+};
 
 module.exports = {
-          connection,
-          query
-}
+  connection,
+  query,
+};
